9d70066250a7911353ecde635e4ba5784abd81e7
fredyolha <fredyolha@gmail.com>
2020-11-01 17:42:03 +0100

    Adding javascript

---
+++ javascript/bottom-cloud.js
@@ -0,0 +1,19 @@
+var widthBottom = $(window).width()-150;
+var velocity = 5000;
+function goRight() {
+    $(".bottom-cloud").animate({
+    left: widthBottom-50
+  }, 8000, function() {
+     setTimeout(goLeft, 50);
+  });
+}
+function goLeft() {
+    $(".bottom-cloud").animate({
+    left: 50
+  }, 8000, function() {
+     setTimeout(goRight, 50);
+  });
+}
+
+
+setTimeout(goRight, 50);
